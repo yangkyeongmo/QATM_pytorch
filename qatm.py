19acda7339caff7cf9b636adcfa1e9ff0d7331e9
Nathan Gyeongmo Yang <nathan.yang@buzzvil.com>
2022-03-10 17:40:51 +0900

    fix: fix code to load model, save in result dir

--- qatm.py
+++ qatm.py
@@ -3,6 +3,7 @@ import torch
 import torchvision
 from torchvision import models, transforms, utils
 import argparse
+import pickle
 from utils import *
 
 import time
@@ -30,6 +31,8 @@ exec(code,  module.__dict__)
 from mod import *
 # -
 
+RESULT_DIR = 'result'
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='QATM Pytorch Implementation')
     parser.add_argument('--cuda', action='store_true')
@@ -42,17 +45,20 @@ if __name__ == '__main__':
     template_dir = args.template_images_dir
     image_path = args.sample_image
     dataset = ImageDataset(Path(template_dir), image_path, thresh_csv='thresh_template.csv')
-    
+
+    start_time = time.time()
     print("define model...")
-    model = CreateModel(model=models.vgg19(pretrained=True).features, alpha=args.alpha, use_cuda=args.cuda)
+    # model = CreateModel(model=models.vgg19(pretrained=True).features, alpha=args.alpha, use_cuda=args.cuda)
+    model = pickle.load(open('model.obj', 'rb'))
+    print(f'took {time.time() - start_time}s.')
+
     print("calculate score...")
-    for _ in range(5):
-        start_time = time.time()
-        scores, w_array, h_array, thresh_list = run_multi_sample(model, dataset)
-        print("nms...")
-        print(f'took {time.time() - start_time}s.')
-        boxes, indices = nms_multi(scores, w_array, h_array, thresh_list)
-        _ = plot_result_multi(dataset.image_raw, boxes, indices, show=False, save_name='result.png')
-        print("result.png was saved")
-
-
+    start_time = time.time()
+    scores, w_array, h_array, thresh_list = run_multi_sample(model, dataset)
+    print("nms...")
+    print(f'took {time.time() - start_time}s.')
+    boxes, indices = nms_multi(scores, w_array, h_array, thresh_list)
+    
+    result_path = os.path.join(RESULT_DIR, os.path.splitext(os.path.basename(image_path))[0] + '.png')
+    _ = plot_result_multi(dataset.image_raw, boxes, indices, show=False, save_name=result_path)
+    print(f"{result_path=}")
